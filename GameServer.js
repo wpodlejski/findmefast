684fe2bb5263ba92038fb1566b1e7e2d7065a502
cedricici <cedricici.web@free.fr>
2013-11-14 23:01:27 +0100

    début de la partie socket

--- GameServer.js
+++ GameServer.js
@@ -30,13 +30,12 @@
 
 
 
-	/* constantes */
-	var nbjoueurs=0;
-	var joueurs=[];
+	/********** GESTION DU JEU *************/
 
-	//on ne gere qu'une partie pour l'instant
-	//'partie' indique si la partie en débutée
-	var partie=false;
+	var joueurs=[];
+	var parties=[];
+
+
 
 	//pas très modulable tout ca... a refaire...
 	// prototyper l'objet client?
@@ -68,109 +67,144 @@
 	//	balpos {}
 	//
 
+	//TODO nettoyer régulièrement joueurs et parties
 
 
 	//gestion du serveur de socket
 	//lors de la connexion d'un client
 	io.sockets.on('connection', function (client) {
 
-
+		var joueur, partie;
 		//nombre de joueur maxi ou partie commencée
+		/*
 	    if(nbjoueurs>6 || partie){
-			client.emit("accuse",{num:0,run:run,message:"jeux plein : mode spectateur"});
+			client.emit("accuse",{id:0,message:"jeux plein : mode spectateur"});
 			return;
 	    }
 
 		nbjoueurs+=1;
-		//recherche de l'id le plus petit disponible hormis 1
-		//qui ne peux se deconnecter sans tuer la partie
+		*/
+		//recherche de l'id le plus petit disponible 
+		/*
+		* TODO : Regarder les limites de cette fonction a +++ joueurs
+		*/
 		var id=1;
 		var i=0;
 		while(i<=joueurs.length){
 			if(!joueurs[i]){
 				id=i+1;
-				joueurs[id-1]={num:id,client:client,pseudo:'',ready:0,score:0};
-				//master=n;
+
+				joueur ={id:id,master:false,pseudo:'',ready:0,score:0};
+				joueurs[id-1]=joueur;
+
 				break;
 			}
 			i++;
 		}
-
-		console.log(nbjoueurs+" joueurs");
-
+		
+		console.log("Connexion d'un nouveau joueur id="+id);
+		
 
 		//accusé de reception de la connexion
 		client.emit("accuse",{num:id,message:"ok game"});
 
 
 
+
 		/************ partie asynchrone ****************/
 
-		client.on('pseudo', function(data) {
+		client.on('creerPartie', function(data) {
+			//le joueur devient master de sa partie
+			joueur.master=true;
+			joueur.pseudo=data.nomJoueur;
 
-			console.log(data.pseudo+" s'est inseré dans le jeu");
-			joueurs[id-1].pseudo=data.pseudo;
+			partie={
+				"id":parties.length,
+				"nom":data.nomPartie,
+				"joueurs":[id]
 
-			client.broadcast.emit("joueur",{num:id,pseudo:data.pseudo,message:""});
+			};
 
-		});
+			console.log(partie);
 
-		client.on('ready', function(data) {
-			joueurs[id-1].ready=1;
+			parties.push(partie);
 
-			client.broadcast.emit("ready",{num:id});
 
-			//on vérifie que tous les joueurs sont prêt
-			/*
-			var okstart=true;
-			for(var i=1;i<5;i++){
-				console.log(joueurs[i]+" "+ready[i]);
-				if(joueurs[i] && !ready[i]){
-					okstart=false;
-				}
-			}
-			if(okstart){
-				//le master peut lancer le jeu
-				console.log("envois du message au master");
-				joueurs[master].emit("startable");
+			console.log(joueur.pseudo+" créé le nouveau jeu "+partie.nom);
 
-			}
-			*/
+			client.emit("jeu",{partie:partie,joueur:joueur});
+			
 		});
 
 
-		client.on('start', function(data) {
-			//normalement, seul le master peut envoyer ce message
-			//les autres joueurs sont cencés être ready
-			client.emit("start",coords);
-			client.broadcast.emit("start",coords);
-			partie=true;
+		client.on('listerParties', function(data) {
+			console.log("listage des parties");
+			client.emit("listeParties",{parties:parties});
+		});
 
+
+
+
+
+
+		client.on('rejoindrePartie', function(data) {
+
+			partie=cherchePartie(data.id);
+			if(partie==null){
+				client.emit("jeu",{erreur:"Partie non trouvée"});
+			}else{
+				//TODO : tester les place restantes dans la partie
+				//TODO : un joueur ne peut pas être 2 fois dans la partie
+				//TODO : un oueur ne peux pas ouer 2 parties en me temps
+
+				//ajout le joueur à la partie
+				partie.joueurs.push(joueur.id);
+				client.emit("jeu",{partie:partie,joueur:joueur});
+				console.log("le joueur "+joueur.pseudo+" rejoint la partie n°"+data.id);
+
+				//broadcast d'ajout de joueur
+				//TODO IMPORTANT
+				//créer des groupes de broadcast pour uniquement es joueurs de la partie 
+				client.broadcast.emit("joueur",{joueur:joueur});
+
+
+			}
+		
 		});
 
 
 		client.on('disconnect', function() {
 			console.log("deconnexion du joueur "+id);
 			if(id==1){
-				//fin de partie
-				client.broadcast.emit("fin",{num:0,message:""});
-				//on vide la partie
-				joueurs=[];
-				nbjoueurs=0;
-				partie=false;
+				//si c'est le master on vire la partie sinon juste le joueur
+				//TODO
+				joueur={};
+				partie={};
 				
+
+				client.broadcast.emit("fin",{});
 				return;
 				//que devient l'instance en cours????
 				//comment la flusher???
 			}
 
-			nbjoueurs-=1;
-			joueurs[id-1]=false;
-
-			console.log(nbjoueurs+" joueurs maintenant:");
-			//annonce a tous le monde d'un joueur en moins
 			client.broadcast.emit("joueur",{num:-id,message:""});
 		});
+
+
+		/***************** Fonctions *************************/
+
+		cherchePartie = function(id){
+			for (var i = 0; i < parties.length; i++) {
+
+				var p = parties[i];
+				console.log(p.id+"/"+id);
+
+				if(p.id==id)return p;
+			}
+			return null;
+		}
+
 
 	});
 })();
--- public/game.js
+++ public/game.js
@@ -8,6 +8,8 @@ GAME.gameStart = (function(){
 
 
 	var pageCourante="#jeu";
+	var id=0;
+
 	/*
 	var pong; //le thread
 	var width=800;
@@ -58,21 +60,50 @@ GAME.gameStart = (function(){
 
 	/****************** Partie socket **************************/
 
-
+	//accusé de reception de la connexion au serveur
+	//TODO : surtout utile en cas de non connexion !!! 
 	sock.on("accuse",function(data){
-		console.log(data);
+		//console.log(data);
+		id=data.id;
 	});
 
 
-	sock.on("message",function(data){
-		console.log(data);
+	sock.on("jeu",function(data){
+		if(data.erreur){
+			alert("Erreur :"+data.erreur);
+			return;
+		}
+		console.log(data.partie.id);
+
+		document.getElementById("pseudo").innerHTML=data.joueur.pseudo;
+		switchPage("#jeu");
+
+		//passage en mode attente
+		document.querySelector("#jeu nav .pret .icon").style.background='red';	//passage en mode attente
+
+
 	});
 
 	// message reçu lorsqu'un joueur est prêt.
-	sock.on("ready",function(data){
-		console.log("Le joueur "+data.num+" est prêt!");
+	sock.on("listeParties",function(data){
 		console.log(data);
+
+		var options='';
+		for(var i=0;i<data.parties.length;i++){
+			options+='<option value="'+data.parties[i].id+'" >'+data.parties[i].nom+
+			'('+data.parties[i].joueurs.length+')</option>';
+		}
+		document.getElementById("liste_parties").innerHTML=options;
+
+		switchPage("#lister_parties");
 	});
+
+
+
+
+///////////////////////////////////////////////////
+
+
 
 	// message reçu par le master lorsque tous les joueur sont prêt
 	sock.on("startable",function(data){
@@ -90,52 +121,61 @@ GAME.gameStart = (function(){
 
 
 
-/* fonctions */
+	/* fonctions */
 
-var switchPage = function(newpage){
-	var courante = document.querySelector(pageCourante);
-	courante.style.zIndex = 0;
-	var nouvelle = document.querySelector(newpage);
-	nouvelle.style.zIndex = 1;
-	pageCourante=newpage;
+	var switchPage = function(newpage){
+		var courante = document.querySelector(pageCourante);
+		//courante.style.zIndex = 0;
+		courante.style.left = '-100%';
+		var nouvelle = document.querySelector(newpage);
+		//nouvelle.style.zIndex = 1;
+		nouvelle.style.left = '0';
+		pageCourante=newpage;
 
-}
-
-
-/****************** Partie interraction *********************/
-
+	}
 
 
-//page accueil
-var liens = document.querySelectorAll('a');
+	/****************** Partie interraction *********************/
 
-console.log(liens);
 
-for (var i = 0; i < liens.length; i++) {
- 	liens[i].addEventListener("click",function(event){
- 		event.preventDefault();
- 		var target = event.currentTarget.attributes['href'].nodeValue || '#accueil';
-		console.log(target)
 
-		if(target=="#accueil"){
-			switchPage(target);
-		}
-		if(target=="#creer_partie"){
-			switchPage(target);
-		}
-		if(target=="#rejoindre_partie"){
-			switchPage(target);
-		}
-		if(target=="#options"){
-			switchPage(target);
-		}
-		if(target=="#accueil"){
-			//switchPage(target);
-		}
+	////gestion des différents liens
+	var liens = document.querySelectorAll('a');
+	for (var i = 0; i < liens.length; i++) {
+	 	liens[i].addEventListener("click",function(event){
+	 		event.preventDefault();
+	 		var target = event.currentTarget.attributes['href'].nodeValue || '#accueil';
+			console.log(target)
 
- 	},false);
-}
+			if(target=="#accueil"){
+				switchPage(target);
+			}
+			if(target=="#nouvelle_partie"){
+				switchPage(target);
+			}
+			if(target=="#lister_parties"){
+				//récuperer les parties
+				sock.emit("listerParties",{});
+				//TODO : afficher un sablier
+			}
+			if(target=="#options"){
+				switchPage(target);
+			}
+			if(target=="creer_partie"){
+				var nomJoueur = document.getElementById("nom_joueur").value;
+				var nomPartie = document.getElementById("nom_partie").value;
 
+				sock.emit("creerPartie",{nomJoueur:nomJoueur,nomPartie:nomPartie});
+				//TODO : afficher un sablier
+			}
+			if(target=="rejoindre_partie"){ //
+				var id = document.getElementById("liste_parties").value;
+				sock.emit("rejoindrePartie",{id:id});
+				//TODO : afficher un sablier
+			}
+
+		},false);
+	}
 
 });
 
